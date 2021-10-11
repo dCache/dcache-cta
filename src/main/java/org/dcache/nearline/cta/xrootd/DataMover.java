fabd4a01d60f45b19bbc0497a236da8f4176ff4d
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2021-10-11 17:27:55 +0200

    fix xrootd authx:none initialization

--- src/main/java/org/dcache/nearline/cta/xrootd/DataMover.java
+++ src/main/java/org/dcache/nearline/cta/xrootd/DataMover.java
@@ -3,6 +3,7 @@ package org.dcache.nearline.cta.xrootd;
 import static org.dcache.xrootd.protocol.XrootdProtocol.DATA_SERVER;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
 import com.google.common.util.concurrent.AbstractIdleService;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
@@ -15,16 +16,27 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
 import java.net.InetSocketAddress;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Properties;
+import java.util.ServiceLoader;
+import org.dcache.xrootd.core.XrootdAuthenticationHandlerProvider;
+import org.dcache.xrootd.core.XrootdAuthorizationHandlerProvider;
 import org.dcache.xrootd.core.XrootdDecoder;
 import org.dcache.xrootd.core.XrootdEncoder;
 import org.dcache.xrootd.core.XrootdHandshakeHandler;
+import org.dcache.xrootd.plugins.ChannelHandlerFactory;
+import org.dcache.xrootd.plugins.ChannelHandlerProvider;
 import org.dcache.xrootd.stream.ChunkedResponseWriteHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DataMover extends AbstractIdleService {
 
-    private static final Logger logger = LoggerFactory.getLogger(DataMover.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataMover.class);
 
     private ServerBootstrap server;
     private ChannelFuture cf;
@@ -34,16 +46,21 @@ public class DataMover extends AbstractIdleService {
 
     public DataMover(InetSocketAddress sa) {
 
-        bossGroup = new NioEventLoopGroup();
-        workerGroup = new NioEventLoopGroup();
+        try {
+            bossGroup = new NioEventLoopGroup();
+            workerGroup = new NioEventLoopGroup();
 
-        server = new ServerBootstrap()
-              .group(bossGroup, workerGroup)
-              .channel(NioServerSocketChannel.class)
-              .localAddress(sa)
-              .childOption(ChannelOption.TCP_NODELAY, true)
-              .childOption(ChannelOption.SO_KEEPALIVE, true)
-              .childHandler(new XrootChallenInitializer());
+            server = new ServerBootstrap()
+                  .group(bossGroup, workerGroup)
+                  .channel(NioServerSocketChannel.class)
+                  .localAddress(sa)
+                  .childOption(ChannelOption.TCP_NODELAY, true)
+                  .childOption(ChannelOption.SO_KEEPALIVE, true)
+                  .childHandler(new XrootChallenInitializer());
+        } catch (Exception e) {
+            Throwables.throwIfUnchecked(e);
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -71,18 +88,57 @@ public class DataMover extends AbstractIdleService {
 
     private static class XrootChallenInitializer extends ChannelInitializer<Channel> {
 
+        public final List<ChannelHandlerFactory> channelHandlerFactories;
+        private final ServiceLoader<ChannelHandlerProvider> channelHandlerProviders;
+
+        XrootChallenInitializer() throws Exception {
+
+            var pluginLoader =  this.getClass().getClassLoader();
+            XrootdAuthenticationHandlerProvider.setPluginClassLoader(pluginLoader);
+            XrootdAuthorizationHandlerProvider.setPluginClassLoader(pluginLoader);
+
+            channelHandlerProviders =
+                  ServiceLoader.load(ChannelHandlerProvider.class, pluginLoader);
+
+            channelHandlerFactories = new ArrayList<>();
+            for (String plugin: List.of("authn:none")) {
+                channelHandlerFactories.add(createHandlerFactory(plugin));
+            }
+        }
+
         @Override
         protected void initChannel(Channel ch) throws Exception {
             ChannelPipeline pipeline = ch.pipeline();
             pipeline.addLast("handshaker", new XrootdHandshakeHandler(DATA_SERVER));
             pipeline.addLast("encoder", new XrootdEncoder());
             pipeline.addLast("decoder", new XrootdDecoder());
-            if (logger.isDebugEnabled()) {
+
+            if (LOGGER.isDebugEnabled()) {
                 pipeline.addLast("logger", new LoggingHandler(XrootChallenInitializer.class));
+            }
+
+            for (ChannelHandlerFactory factory: channelHandlerFactories) {
+                pipeline.addLast("plugin:" + factory.getName(), factory.createHandler());
             }
 
             pipeline.addLast("chunk-writer", new ChunkedResponseWriteHandler());
             pipeline.addLast("data-server", new DataServerHandler());
+        }
+
+        public final ChannelHandlerFactory createHandlerFactory(String plugin)
+              throws Exception
+        {
+            for (ChannelHandlerProvider provider: channelHandlerProviders) {
+                ChannelHandlerFactory factory =
+                      provider.createFactory(plugin, new Properties());
+                if (factory != null) {
+                    LOGGER.debug("ChannelHandler plugin {} is provided by {}", plugin, provider.getClass());
+                    return factory;
+                } else {
+                    LOGGER.debug("ChannelHandler plugin {} could not be provided by {}", plugin, provider.getClass());
+                }
+            }
+            throw new NoSuchElementException("Channel handler plugin not found: " + plugin);
         }
 
     }
