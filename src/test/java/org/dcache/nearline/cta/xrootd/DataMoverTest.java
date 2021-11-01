1c2eb56d875129c7e0eea137dffc51ab5cef55d2
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2021-11-01 16:17:04 +0100

    xroot: add initial tests for DataMover

---
+++ src/test/java/org/dcache/nearline/cta/xrootd/DataMoverTest.java
@@ -0,0 +1,55 @@
+package org.dcache.nearline.cta.xrootd;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyOrNullString;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.UnknownHostException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import org.dcache.pool.nearline.spi.NearlineRequest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DataMoverTest {
+
+    private DataMover dataMover;
+    private ConcurrentMap<String, NearlineRequest> requests;
+
+    @Before
+    public void setUp() throws UnknownHostException {
+        requests = new ConcurrentHashMap<>();
+        dataMover = new DataMover("cta", "testing",
+              new InetSocketAddress(InetAddress.getLocalHost(), 0),
+              requests);
+        dataMover.startAsync().awaitRunning();
+    }
+
+    @After
+    public void tearDown() {
+        dataMover.stopAsync().awaitTerminated();
+    }
+
+    @Test(expected = Exception.class)
+    public void testBindError() throws UnknownHostException {
+        URI uri = URI.create(dataMover.getTransport("1").getDstUrl());
+        new DataMover("foo", "bar",
+              new InetSocketAddress(InetAddress.getLocalHost(), uri.getPort()), requests)
+              .startAsync()
+              .awaitRunning();
+    }
+
+    @Test
+    public void testGetTransport() {
+        var transport = dataMover.getTransport("1");
+
+        assertThat("destination IRL is not set", transport.getDstUrl(), not(emptyOrNullString()));
+        assertThat("error report URL is not set", transport.getErrorReportUrl(),
+              not(emptyOrNullString()));
+        assertThat("status URL is not set", transport.getReportUrl(), not(emptyOrNullString()));
+    }
+}
