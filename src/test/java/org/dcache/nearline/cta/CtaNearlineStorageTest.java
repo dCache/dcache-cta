4b0b7c80c56a9acdd4a5d41368a0af30afb75b73
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2023-05-30 12:45:21 +0200

    test: pull out test cert generation into utility class

--- src/test/java/org/dcache/nearline/cta/CtaNearlineStorageTest.java
+++ src/test/java/org/dcache/nearline/cta/CtaNearlineStorageTest.java
@@ -1,8 +1,5 @@
 package org.dcache.nearline.cta;
 
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
-import static java.nio.file.StandardOpenOption.WRITE;
 import static org.dcache.nearline.cta.CtaNearlineStorage.CTA_CA;
 import static org.dcache.nearline.cta.CtaNearlineStorage.CTA_ENDPOINT;
 import static org.dcache.nearline.cta.CtaNearlineStorage.CTA_GROUP;
@@ -11,6 +8,7 @@ import static org.dcache.nearline.cta.CtaNearlineStorage.CTA_REQUEST_TIMEOUT;
 import static org.dcache.nearline.cta.CtaNearlineStorage.CTA_TLS;
 import static org.dcache.nearline.cta.CtaNearlineStorage.CTA_USER;
 import static org.dcache.nearline.cta.CtaNearlineStorage.IO_PORT;
+import static org.dcache.nearline.cta.TestUtils.generateSelfSignedCert;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
@@ -24,19 +22,9 @@ import static org.mockito.Mockito.when;
 import com.google.common.util.concurrent.Futures;
 import diskCacheV111.util.CacheException;
 import diskCacheV111.vehicles.GenericStorageInfo;
-import eu.emi.security.authn.x509.impl.CertificateUtils;
-import eu.emi.security.authn.x509.impl.CertificateUtils.Encoding;
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.math.BigInteger;
 import java.net.URI;
-import java.nio.file.Files;
-import java.security.GeneralSecurityException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.SecureRandom;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -46,20 +34,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import org.bouncycastle.asn1.x500.X500Name;
-import org.bouncycastle.asn1.x509.BasicConstraints;
-import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
-import org.bouncycastle.asn1.x509.Extension;
-import org.bouncycastle.asn1.x509.KeyPurposeId;
-import org.bouncycastle.asn1.x509.KeyUsage;
-import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
-import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.cert.X509v3CertificateBuilder;
-import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.operator.ContentSigner;
-import org.bouncycastle.operator.OperatorCreationException;
-import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.dcache.pool.nearline.spi.FlushRequest;
 import org.dcache.pool.nearline.spi.RemoveRequest;
 import org.dcache.pool.nearline.spi.StageRequest;
@@ -85,7 +59,7 @@ public class CtaNearlineStorageTest {
         keyFile = File.createTempFile("hostkey-", ".pem");
         certFile = File.createTempFile("hostcert-", ".pem");
 
-        generateSelfSignedCert();
+        generateSelfSignedCert(certFile, keyFile);
 
         cta = new DummyCta(certFile, keyFile);
         cta.start();
@@ -781,55 +755,6 @@ public class CtaNearlineStorageTest {
             return null;
         }).when(request).completed(any());
         return request;
-    }
-
-    private void generateSelfSignedCert()
-          throws GeneralSecurityException, OperatorCreationException, IOException {
-
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", new BouncyCastleProvider());
-        keyPairGenerator.initialize(2048, new SecureRandom());
-        KeyPair keyPair = keyPairGenerator.generateKeyPair();
-
-        long notBefore = System.currentTimeMillis();
-        long notAfter = notBefore + TimeUnit.DAYS.toMillis(1);
-
-        X500Name subjectDN = new X500Name("CN=localhost, O=dCache.org");
-        X500Name issuerDN = subjectDN;
-
-        SubjectPublicKeyInfo subjectPublicKeyInfo =
-              SubjectPublicKeyInfo.getInstance(keyPair.getPublic().getEncoded());
-
-        X509v3CertificateBuilder certificateBuilder = new X509v3CertificateBuilder(issuerDN,
-              BigInteger.ONE,
-              new Date(notBefore),
-              new Date(notAfter), subjectDN,
-              subjectPublicKeyInfo)
-              .addExtension(Extension.basicConstraints, true, new BasicConstraints(true))
-              .addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.digitalSignature | KeyUsage.keyEncipherment))
-              .addExtension(Extension.extendedKeyUsage, true, new ExtendedKeyUsage(
-                    new KeyPurposeId[] {KeyPurposeId.id_kp_clientAuth, KeyPurposeId.id_kp_serverAuth}
-                    )
-              );
-
-
-
-        String signatureAlgorithm = "SHA256WithRSA";
-
-        // sign with own key
-        ContentSigner contentSigner = new JcaContentSignerBuilder(signatureAlgorithm)
-              .build(keyPair.getPrivate());
-
-        X509CertificateHolder certificateHolder = certificateBuilder.build(contentSigner);
-        var cert = new JcaX509CertificateConverter().getCertificate(certificateHolder);
-
-        try (OutputStream certOut = Files.newOutputStream(
-              certFile.toPath(), CREATE, TRUNCATE_EXISTING,
-              WRITE); OutputStream keyOut = Files.newOutputStream(keyFile.toPath(), CREATE,
-              TRUNCATE_EXISTING, WRITE)) {
-
-            CertificateUtils.saveCertificate(certOut, cert, Encoding.PEM);
-            CertificateUtils.savePrivateKey(keyOut, keyPair.getPrivate(), Encoding.PEM, null, null);
-        }
     }
 
 }
---
+++ src/test/java/org/dcache/nearline/cta/TestUtils.java
@@ -0,0 +1,88 @@
+package org.dcache.nearline.cta;
+
+import eu.emi.security.authn.x509.impl.CertificateUtils;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.KeyPurposeId;
+import org.bouncycastle.asn1.x509.KeyUsage;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+public final class TestUtils {
+
+    private TestUtils() {}
+
+    public static void generateSelfSignedCert(File certFile, File keyFile)
+            throws GeneralSecurityException, OperatorCreationException, IOException {
+
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", new BouncyCastleProvider());
+        keyPairGenerator.initialize(2048, new SecureRandom());
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        long notBefore = System.currentTimeMillis();
+        long notAfter = notBefore + TimeUnit.DAYS.toMillis(1);
+
+        X500Name subjectDN = new X500Name("CN=localhost, O=dCache.org");
+        X500Name issuerDN = subjectDN;
+
+        SubjectPublicKeyInfo subjectPublicKeyInfo =
+                SubjectPublicKeyInfo.getInstance(keyPair.getPublic().getEncoded());
+
+        X509v3CertificateBuilder certificateBuilder = new X509v3CertificateBuilder(issuerDN,
+                BigInteger.ONE,
+                new Date(notBefore),
+                new Date(notAfter), subjectDN,
+                subjectPublicKeyInfo)
+                .addExtension(Extension.basicConstraints, true, new BasicConstraints(true))
+                .addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.digitalSignature | KeyUsage.keyEncipherment))
+                .addExtension(Extension.extendedKeyUsage, true, new ExtendedKeyUsage(
+                                new KeyPurposeId[] {KeyPurposeId.id_kp_clientAuth, KeyPurposeId.id_kp_serverAuth}
+                        )
+                );
+
+
+
+        String signatureAlgorithm = "SHA256WithRSA";
+
+        // sign with own key
+        ContentSigner contentSigner = new JcaContentSignerBuilder(signatureAlgorithm)
+                .build(keyPair.getPrivate());
+
+        X509CertificateHolder certificateHolder = certificateBuilder.build(contentSigner);
+        var cert = new JcaX509CertificateConverter().getCertificate(certificateHolder);
+
+        try (OutputStream certOut = Files.newOutputStream(
+                certFile.toPath(), CREATE, TRUNCATE_EXISTING,
+                WRITE); OutputStream keyOut = Files.newOutputStream(keyFile.toPath(), CREATE,
+                TRUNCATE_EXISTING, WRITE)) {
+
+            CertificateUtils.saveCertificate(certOut, cert, CertificateUtils.Encoding.PEM);
+            CertificateUtils.savePrivateKey(keyOut, keyPair.getPrivate(), CertificateUtils.Encoding.PEM, null, null);
+        }
+    }
+
+}
