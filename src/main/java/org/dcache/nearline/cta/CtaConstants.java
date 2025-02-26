2f4176587f4464e321ec0f49659aaa0f18f22de0
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2025-02-26 11:23:40 +0100

    drv: ensure that abort always sets the request id

--- src/main/java/org/dcache/nearline/cta/CtaConstants.java
+++ src/main/java/org/dcache/nearline/cta/CtaConstants.java
@@ -4,6 +4,7 @@ public final class CtaConstants {
 
     public static String ATTR_ARCHIVE_ID = "sys.archive.file_id";
     public static String ATTR_OBJECTSTOE_ID = "sys.cta.archive.objectstore.id";
+    public static String ATTR_STORED_OBJECTSTOE_ID = "sys.cta.objectstore.id";
 
     private CtaConstants() {
     }
